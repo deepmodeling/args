0c39e0aa041bccaf3ef590c4504556fcb6d47baf
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-03-20 15:27:47 -0400

    add dpgen2 (#10)
    * add dpgen2
    
    * import dpgen2
    
    * Update generator.py

--- generator.py
+++ generator.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import deepmd
 import dpgen
+import dpgen2
 import dpdispatcher
 from deepmd.utils.argcheck import model_args, learning_rate_args, loss_args, training_args
 from dpgen.generator.arginfo import run_jdata_arginfo, run_mdata_arginfo
@@ -16,6 +17,7 @@ from dpgen.data.arginfo import (
     init_reaction_mdata_arginfo,
 )
 from dpdispatcher import Resources, Task, Machine
+from dpgen2.entrypoint.args import submit_args
 from dargs import ArgumentEncoder, Argument
 
 args_path = Path(__file__).parent / "args"
@@ -195,6 +197,15 @@ add_parameter(
     Task.arginfo(),
     dpdispatcher.__version__,
     "deepmodeling/dpdispatcher",
+)
+
+# DPGEN2
+add_parameter(
+    "dpgen2-submit",
+    "DP-GEN2 Submit",
+    submit_args(),
+    dpgen2.__version__,
+    "deepmodeling/dpgen2",
 )
 
 # args.json
